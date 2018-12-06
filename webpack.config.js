8f5750e6664c13f7299a0d049fc33713acfbbf11
Gustavo Verzola <verzola@gmail.com>
2018-12-06 01:30:19 -0200

    disable notify

--- webpack.config.js
+++ webpack.config.js
@@ -24,7 +24,8 @@ module.exports = {
     new BrowserSyncPlugin({
       host: 'localhost',
       port: 3000,
-      server: { baseDir: ['dist'] }
+      server: { baseDir: ['dist'] },
+      notify: false
     }),
     new HtmlWebpackPlugin({
       template: './src/index.html',
