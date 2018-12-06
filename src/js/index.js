edc90cf660a433f940312d82746a3c1692b3384b
Gustavo Verzola <verzola@gmail.com>
2018-12-06 01:41:08 -0200

    add toggle behavior to hamburger menu

--- src/js/index.js
+++ src/js/index.js
@@ -2,4 +2,6 @@ import '../scss/main.scss';
 import '@fortawesome/fontawesome-free/js/all.js';
 import '@fortawesome/fontawesome-free/scss/fontawesome.scss';
 
-console.log('hello world');
+document.querySelector('.hamburger').addEventListener('click', function() {
+  this.classList.toggle('is-active');
+});
