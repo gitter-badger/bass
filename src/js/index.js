0190931c4f4c973086aeebdb88f8b8f837df5c3b
Gustavo Verzola <verzola@gmail.com>
2018-12-07 00:30:12 -0200

    add offcanvas component

--- src/js/index.js
+++ src/js/index.js
@@ -2,6 +2,8 @@ import '../scss/main.scss';
 import '@fortawesome/fontawesome-free/js/all.js';
 import '@fortawesome/fontawesome-free/scss/fontawesome.scss';
 
-document.querySelector('.hamburger').addEventListener('click', function() {
+document.querySelector('.js-hamburger').addEventListener('click', function() {
   this.classList.toggle('is-active');
+
+  document.querySelector('.js-offcanvas').classList.toggle('c-offcanvas--open');
 });
